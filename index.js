62aaad28a4873e61386ac9b20521e6b646b35144
Никита Кузнецов <nikita.kuznetsov.r@gmail.com>
2023-07-25 20:54:25 +0300

    сдек

--- index.js
+++ index.js
@@ -3,4 +3,14 @@ $(document).ready(function() {
         items: 1,
         loop: true
     })
+
+    $('.js-nav__burger').click(function() {
+        $('.nav__body').addClass('open')
+        $('body').addClass('hidden')
+    })
+
+    $('.js-nav__burger-close').click(function() {
+        $('.nav__body').removeClass('open')
+        $('body').removeClass('hidden')
+    })
 });
